01acde4150239e594c1c580547a700d98de45125
rakibidb <rakibhossan55@gmail.com>
2018-09-04 13:36:12 +0600

    custmize repo interface

--- Microservices/Book/src/main/java/com/mshr/repo/BookRepo.java
+++ Microservices/Book/src/main/java/com/mshr/repo/BookRepo.java
@@ -1,6 +1,7 @@
 package com.mshr.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,5 +10,12 @@ import com.mshr.model.Book;
 @RepositoryRestResource
 @CrossOrigin("*")
 public interface BookRepo extends JpaRepository<Book, Integer> {
+
+	@Override
+	Book save(Book book);
+
+	Book findByTitle(@Param("title") String title);
+
+	Book findByAuthor(@Param("author") String author);
 
 }
