38af3254aecc2645b9f49a0e1d75172ca11f20e1
rakibidb <rakibhossan55@gmail.com>
2018-09-06 18:45:24 +0600

    add login form

--- Frontend/library-management-system/src/app/app.module.ts
+++ Frontend/library-management-system/src/app/app.module.ts
@@ -7,14 +7,30 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 import { DefaultContentComponent } from './default-content/default-content.component';
+import { RouterModule, Route } from '@angular/router';
+import { BookComponent } from './book/book.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+const routes: Route[] = [
+  {path: '', component: NavbarComponent },
+  {path: 'book', component: BookComponent },
+  {path: '**', component: ErrorComponent },
+  // {path: 'view2', component:____ },
+  // {path: 'view3', component: ____ },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DefaultContentComponent
+    DefaultContentComponent,
+    BookComponent,
+    ErrorComponent,
+    LoginFormComponent,
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
@@ -24,7 +40,13 @@ import { DefaultContentComponent } from './default-content/default-content.compo
     MatIconModule,
     MatListModule
   ],
+  exports:[RouterModule],
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
+
+
+
 export class AppModule { }
